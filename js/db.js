28e3613c8803b07ccf88c06230d6670ae1a297ed
Veronica Johnson <ronijohnveronica@gmail.com>
2019-07-30 09:03:14 -0400

    7/30

--- js/db.js
+++ js/db.js
@@ -7,11 +7,31 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "LM21pz0@",
+    password: "password",
+    // password: "LM21pz0@",
     database: "vacay",
 });
 
 con.connect(function(err) {
   if (err) throw err;
+      con.query("SELECT * FROM city", function (err, result, fields) {
+      if (err) throw err;
+      // console.log(result);
   console.log("Connected!");
 });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//       con.query("SELECT * FROM poi", function (err, result, fields) {
+//       if (err) throw err;
+//       // console.log(result);
+//   console.log("Connected!");
+// });
+
+// con.connect(function(err) {
+//     if (err) throw err;
+//     con.query("SELECT * FROM inventory", function (err, result, fields) {
+//       if (err) throw err;
+//       console.log(result);
+//     });
+//   });
